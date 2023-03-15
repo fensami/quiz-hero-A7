01ac6386f283a7074df99683865ccaeb8e9a1eb0
fensami <113832498+fensami@users.noreply.github.com>
2023-03-15 22:58:07 +0600

    71 line question spelling was wrong

--- index.js
+++ index.js
@@ -53,20 +53,22 @@ const loadQuiz = async () => {
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
+  // console.log(data);
   if (!data) {
     // quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
-    const queryContainer = document.createElement('div')
-    queryContainer.classList.add('quizContainer')
+    // console.log(quiz);
+    // const queryContainer = document.createElement('div')
+    // queryContainer.classList.add('quizContainer')
     quizContainer.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
